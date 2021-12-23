c6a13e01234188f7228d0bb0b8325674a255913e
KuzVlad <kuzvlad1993@gmail.com>
2021-12-23 17:02:27 +0300

    version 1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -55,43 +55,43 @@ int main()
     else
         return 0;
 
-        std::shared_ptr<hello_info> info = list[index]->get_info();
+    std::shared_ptr<hello_info> info = list[index]->get_info();
 
-        // Print information about the scanner to which the profile belongs.
-        std::cout << "-----------------------------------------" << std::endl;
-        std::cout << "Device information: "                      << std::endl;
-        std::cout << "* Name  \t: "   << info->device_name()     << std::endl;
-        std::cout << "* Serial\t: "   << info->serial_number()   << std::endl;
-        std::cout << "* IP Addr\t: "  << info->ip_address()      << std::endl;
+    // Print information about the scanner to which the profile belongs.
+    std::cout << "-----------------------------------------" << std::endl;
+    std::cout << "Device information: "                      << std::endl;
+    std::cout << "* Name  \t: "   << info->device_name()     << std::endl;
+    std::cout << "* Serial\t: "   << info->serial_number()   << std::endl;
+    std::cout << "* IP Addr\t: "  << info->ip_address()      << std::endl;
 
-        // Establish connection to the RF627 device by Service Protocol.
-        bool is_connected = list[index]->connect();
+    // Establish connection to the RF627 device by Service Protocol.
+    bool is_connected = list[index]->connect();
 
-        if (is_connected)
-        {
-            std::thread receiver = std::thread(receive_profiles, list[index]);
+    if (is_connected)
+    {
+        std::thread receiver = std::thread(receive_profiles, list[index]);
 #ifdef _WIN32
-            SetThreadPriority(receiver.native_handle(), THREAD_PRIORITY_HIGHEST);
+        SetThreadPriority(receiver.native_handle(), THREAD_PRIORITY_HIGHEST);
 #endif
 
-            while (true) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-                std::cout << "FPS: " << profile_count
-                          << "; Lost: " << profile_lost
-                          << "; MinTimeDiff: " << min_time_diff
-                          << "; MaxTimeDiff: "<< max_time_diff << std::endl;
-                profile_lost = 0;
-                profile_count = 0;
-                min_time_diff = -1;
-                max_time_diff = 0;
-            }
-
-            if (receiver.joinable())
-                receiver.join();
-
-            // Disconnect from scanner.
-            list[index]->disconnect();
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            std::cout << "FPS: " << profile_count
+                      << "; Lost: " << profile_lost
+                      << "; MinTimeDiff: " << min_time_diff
+                      << "; MaxTimeDiff: "<< max_time_diff << std::endl;
+            profile_lost = 0;
+            profile_count = 0;
+            min_time_diff = -1;
+            max_time_diff = 0;
         }
+
+        if (receiver.joinable())
+            receiver.join();
+
+        // Disconnect from scanner.
+        list[index]->disconnect();
+    }
 
     // Cleanup resources allocated with sdk_init()
     sdk_cleanup();
@@ -107,33 +107,33 @@ void receive_profiles(std::shared_ptr<rf627smart> scanner)
     int last_index = -1;
     uint64_t last_time = 0;
     while(true)
-    if ((profile = scanner->get_profile2D(zero_points,realtime)))
-    {
-        if (last_index == -1)
+        if ((profile = scanner->get_profile2D(zero_points,realtime)))
         {
-            last_time = profile->getHeader().system_time;
-            last_index = profile->getHeader().measure_count;
-        }
-        else
-        {
-            profile_count++;
-            if (profile->getHeader().measure_count - last_index > 1)
-                profile_lost += profile->getHeader().measure_count - last_index;
+            if (last_index == -1)
+            {
+                last_time = profile->getHeader().system_time;
+                last_index = profile->getHeader().measure_count;
+            }
             else
             {
-                if (min_time_diff > profile->getHeader().system_time - last_time)
-                    min_time_diff = profile->getHeader().system_time - last_time;
-                if (max_time_diff < profile->getHeader().system_time - last_time)
-                    max_time_diff = profile->getHeader().system_time - last_time;
+                profile_count++;
+                if (profile->getHeader().measure_count - last_index > 1)
+                    profile_lost += profile->getHeader().measure_count - last_index;
+                else
+                {
+                    if (min_time_diff > profile->getHeader().system_time - last_time)
+                        min_time_diff = profile->getHeader().system_time - last_time;
+                    if (max_time_diff < profile->getHeader().system_time - last_time)
+                        max_time_diff = profile->getHeader().system_time - last_time;
+                }
+                last_index = profile->getHeader().measure_count;
+                last_time = profile->getHeader().system_time;
             }
-            last_index = profile->getHeader().measure_count;
-            last_time = profile->getHeader().system_time;
+        }else
+        {
+            std::cout << "Profile was not received!"                 <<std::endl;
+            std::cout << "-----------------------------------------" <<std::endl;
         }
-    }else
-    {
-        std::cout << "Profile was not received!"                 <<std::endl;
-        std::cout << "-----------------------------------------" <<std::endl;
-    }
 }
 
 
